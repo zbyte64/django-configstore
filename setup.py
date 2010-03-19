b6072d108f089a1557379eee4fc96cc11c989a86
Jason Kraus <JasonK@cukerinteractive.com>
2010-03-19 11:18:27 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ VERSION = "0.1.alpha"
 
 setup(name='django-configstore',
       version=VERSION,
-      short_description='An application to allow for other apps to easily store site based configurations',
-      description=LONG_DESCRIPTION,
+      description='An application to allow for other apps to easily store site based configurations',
+      long_description=LONG_DESCRIPTION,
       author='Jason Kraus',
       author_email='zbyte64@gmail.com',
-      url='',
+      url='http://github.com/cuker/django-configstore',
       packages=find_packages(exclude=['ez_setup', 'test', 'tests']),
       test_suite='tests.runtests.runtests',
      )
